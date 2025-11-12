7952b80d819f790229c5268665731b9af08aa8f3
github-action-benchmark <github@users.noreply.github.com>
2025-11-12 19:04:36 +0000

    add Brillig Bytecode Size (customSmallerIsBetter) benchmark result for 054253635fceb839fdf2bb3fd204883efeecbd83

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762974275759,
+  "lastUpdate": 1762974276788,
   "repoUrl": "https://github.com/noir-lang/noir_check_shuffle",
   "entries": {
     "ACIR Opcodes": [
@@ -60,6 +60,37 @@ window.BENCHMARK_DATA = {
             "name": "bench_shuffle.json/main",
             "value": 2853,
             "unit": "circuit_size"
+          }
+        ]
+      }
+    ],
+    "Brillig Bytecode Size": [
+      {
+        "commit": {
+          "author": {
+            "email": "15848336+TomAFrench@users.noreply.github.com",
+            "name": "Tom French",
+            "username": "TomAFrench"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "054253635fceb839fdf2bb3fd204883efeecbd83",
+          "message": "chore: Add permissions for write access in benchmark workflow (#10)\n\n* chore: Add permissions for write access in benchmark workflow\n\n* chore: Add pull-requests permission to benchmark workflow",
+          "timestamp": "2025-11-12T11:04:14-08:00",
+          "tree_id": "d2d60eb3a6b9ea1ec3dbbf34bca6c2e809dbc685",
+          "url": "https://github.com/noir-lang/noir_check_shuffle/commit/054253635fceb839fdf2bb3fd204883efeecbd83"
+        },
+        "date": 1762974276774,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "bench_shuffle",
+            "value": 264,
+            "unit": "opcodes"
           }
         ]
       }
