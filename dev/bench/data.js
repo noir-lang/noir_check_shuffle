72d1aab759bea96aa686f2fab05a3dde2acefc6d
github-action-benchmark <github@users.noreply.github.com>
2025-11-12 19:04:34 +0000

    add ACIR Opcodes (customSmallerIsBetter) benchmark result for 054253635fceb839fdf2bb3fd204883efeecbd83

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1762974274824,
+  "repoUrl": "https://github.com/noir-lang/noir_check_shuffle",
+  "entries": {
+    "ACIR Opcodes": [
+      {
+        "commit": {
+          "author": {
+            "email": "15848336+TomAFrench@users.noreply.github.com",
+            "name": "Tom French",
+            "username": "TomAFrench"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "054253635fceb839fdf2bb3fd204883efeecbd83",
+          "message": "chore: Add permissions for write access in benchmark workflow (#10)\n\n* chore: Add permissions for write access in benchmark workflow\n\n* chore: Add pull-requests permission to benchmark workflow",
+          "timestamp": "2025-11-12T11:04:14-08:00",
+          "tree_id": "d2d60eb3a6b9ea1ec3dbbf34bca6c2e809dbc685",
+          "url": "https://github.com/noir-lang/noir_check_shuffle/commit/054253635fceb839fdf2bb3fd204883efeecbd83"
+        },
+        "date": 1762974274019,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "bench_shuffle.json/main",
+            "value": 68,
+            "unit": "acir_opcodes"
+          }
+        ]
+      }
+    ]
+  }
+}
